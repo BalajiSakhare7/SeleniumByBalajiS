d571819f1601660f1d963af483c0ffab6b531f69
MaliPatil <MaliPatil@MaliPatil-PC>
2019-07-25 22:33:44 +0530

    Second commit

--- TestNgFramework/src/main/java/app.java
+++ TestNgFramework/src/main/java/app.java
@@ -4,6 +4,7 @@ public class app {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("this is app");
+			System.out.println("making changes to commit in git repo");
 	}
 
 }
