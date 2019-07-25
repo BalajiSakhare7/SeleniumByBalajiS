6b96d7a19bfb26d012fa032c082fae6110181d9d
MaliPatil <MaliPatil@MaliPatil-PC>
2019-07-25 23:04:22 +0530

    third commit

--- TestNgFramework/src/main/java/app.java
+++ TestNgFramework/src/main/java/app.java
@@ -1,10 +1,10 @@
 
 public class app {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("this is app");
-			System.out.println("making changes to commit in git repo");
-	}
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * System.out.println("this is app");
+	 * System.out.println("making changes to commit in git repo"); }
+	 */
 
 }
